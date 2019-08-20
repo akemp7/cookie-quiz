ed32acd6ccf72d7aa8d7a0d08785ae26af92f16c
Anita Kemp and Jacqueline Remmel <anita@>
2019-08-20 10:59:35 -0700

    Add three cookie result options and if statement for oatmeal cookie result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $("#quiz").submit(function(event){
     event.preventDefault();
+    $(".result").hide();
     var foodArray = [];
     $("input:checkbox[name=foods]:checked").each(function() {
       var food = $(this).val();
@@ -16,13 +17,20 @@ $(document).ready(function(){
       var pet = $(this).val();
       petArray.push(pet);
     });
-    console.log(foodArray);
-    console.log(colorArray);
-    console.log(petArray);
+
+    if (foodArray.includes("oatmeal") && foodArray.includes("raisins")) {
+      $("#or").show();
+    } else if () {
+      $("#cc").show();
+    } else if () {
+      $("#sugar").show();
+    } else {
+      $("#cc").show();
+    }
 
   });
   $("#clear").click(function(){
     $("#quiz").trigger("reset");
-    $("#output").text("");
+    $(".result").hide();
   });
 });
