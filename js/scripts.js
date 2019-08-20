4b5f4cf491f9111c1dcc4ca43781d214fa56a130
Anita Kemp and Jacqueline Remmel <jacqueline@>
2019-08-20 10:31:56 -0700

    Collect user responses and store in arrays in JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
   $("#quiz").submit(function(event){
     event.preventDefault();
-
+    var foodArray = [];
+    $("input:checkbox[name=foods]:checked").each(function() {
+      var food = $(this).val();
+      foodArray.push(food);
+    });
+    var colorArray = [];
+    $("input:checkbox[name=color]:checked").each(function() {
+      var color = $(this).val();
+      colorArray.push(color);
+    });
+    var petArray = [];
+    $("input:checkbox[name=pets]:checked").each(function() {
+      var pet = $(this).val();
+      petArray.push(pet);
+    });
+    console.log(foodArray);
+    console.log(colorArray);
+    console.log(petArray);
 
   });
-
-
+  $("#clear").click(function(){
+    $("#quiz").trigger("reset");
+    $("#output").text("");
+  });
 });
